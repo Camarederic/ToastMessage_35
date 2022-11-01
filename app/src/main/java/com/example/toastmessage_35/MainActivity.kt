c89ff758bb6f0929d019c19c9789067fc2ccb5d5
Camarederic <camarederic@mail.ru>
2022-11-01 22:37:06 +0300

    Create a Toast message

--- app/src/main/java/com/example/toastmessage_35/MainActivity.kt
+++ app/src/main/java/com/example/toastmessage_35/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.toastmessage_35
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonShowToast.setOnClickListener {
+            Toast.makeText(this, "Hi, I'm a Toast message!", Toast.LENGTH_LONG).show()
+        }
     }
 }
